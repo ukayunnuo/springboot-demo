72627c5dc5947cda8fb80efbb47c82e54707c263
yunnuo <2552846359@qq.com>
2023-09-12 19:32:39 +0800

    init

---
+++ springboot-log4j2-log-demo/src/main/java/com.ukayunnuo/Log4j2DemoApp.java
@@ -0,0 +1,18 @@
+package com.ukayunnuo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * 启动类
+ *
+ * @author yunnuo
+ * @since 1.0.0
+ */
+@SpringBootApplication
+public class Log4j2DemoApp {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Log4j2DemoApp.class, args);
+    }
+}
