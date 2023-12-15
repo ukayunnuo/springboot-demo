5b16a4483f78fc7cbee20b9e0387133c112c5609
yunnuo <2552846359@qq.com>
2023-12-15 18:20:14 +0800

    pref

--- springboot-mp-demo/src/main/java/com/ukayunnuo/domain/request/UserPageReq.java
+++ springboot-mp-demo/src/main/java/com/ukayunnuo/domain/request/UserPageReq.java
@@ -2,6 +2,7 @@ package com.ukayunnuo.domain.request;
 
 import com.ukayunnuo.domain.entity.User;
 import lombok.Data;
+import lombok.EqualsAndHashCode;
 
 /**
  * user page 请求 dto
@@ -9,6 +10,7 @@ import lombok.Data;
  * @author yunnuo <a href="2552846359@qq.com">Email: 2552846359@qq.com</a>
  * @date 2023-09-13
  */
+@EqualsAndHashCode(callSuper = true)
 @Data
 public class UserPageReq extends User {
 
