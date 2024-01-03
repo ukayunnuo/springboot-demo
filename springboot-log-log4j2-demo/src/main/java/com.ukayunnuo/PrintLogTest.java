e25ebea9822c7124cdb276381df0a27de28f0dd1
yunnuo <2552846359@qq.com>
2024-01-03 10:54:44 +0800

    pref: log4j2 model README.md

--- springboot-log-log4j2-demo/src/main/java/com.ukayunnuo/PrintLogTest.java
+++ springboot-log-log4j2-demo/src/main/java/com.ukayunnuo/PrintLogTest.java
@@ -3,9 +3,7 @@ package com.ukayunnuo;
 
 import com.alibaba.fastjson2.JSONObject;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.stereotype.Component;
 
-import javax.annotation.PostConstruct;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -16,25 +14,20 @@ import java.util.Map;
  * @since 1.0.0
  */
 @Slf4j
-@Component
 public class PrintLogTest {
 
+    public static void main(String[] args) {
+        log.info("=============================日志测试打印======================================");
 
-    @PostConstruct
-    public void printLogTest(){
+        log.info("日志测试-->INFO");
+        log.warn("日志测试-->WARN");
+        log.error("日志测试-->ERROR");
 
-            log.info("=============================日志测试打印======================================");
-
-            log.info("日志测试-->INFO");
-            log.warn("日志测试-->WARN");
-            log.error("日志测试-->ERROR");
-
-            Map<String, Object> map = new HashMap<>(3);
-            map.put("name", "yunnuo");
-            map.put("age", 23);
-            map.put("email", "2552846359@qq.com");
-            log.info("author info:{}", JSONObject.toJSONString(map));
+        Map<String, Object> map = new HashMap<>(3);
+        map.put("name", "yunnuo");
+        map.put("age", 23);
+        map.put("email", "2552846359@qq.com");
+        log.info("author info:{}", JSONObject.toJSONString(map));
     }
-
 
 }
